7d71c8e47e885053b1f33b3d0969fff1a52062aa
v.sampreeth <v.sampreeth31@gmail.com>
2021-06-19 19:25:23 +0530

    Intial Commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+
+            return Ok(products);
+        }
+        
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            return Ok(product);
+        }
+    }
+}
