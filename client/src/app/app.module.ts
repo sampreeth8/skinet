898c51161524e0ea67f580ad10b79b5d44585a7f
v.sampreeth <v.sampreeth31@gmail.com>
2021-06-28 12:09:33 +0530

    End of section 10

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,18 +5,24 @@ import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CoreModule } from './core/core.module';
+import { ShopModule } from './shop/shop.module';
+import { HomeModule } from './home/home.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    CoreModule,
+    HomeModule
 
   ],
   providers: [],
